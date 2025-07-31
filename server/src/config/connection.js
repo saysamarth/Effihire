9e66caecbced86bf9503f643cfe8cd323f896b96
Samarth Sharma <samarth@Samarths-MacBook-Air.local>
2025-07-31 13:24:36 +0530

    update

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port: process.env.DB_PORT || 5432,
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
